e4aa8846f516703f25ae5e29044df1b5b92fb91f
root <root@lamp>
2017-04-19 17:44:02 +0000

    Routes for Tutor/Student sessions page and Tutor/Student profile page

--- routes.php
+++ routes.php
@@ -1,10 +1,34 @@
 <?php
 // Routes
-
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//View Tutor Profile
+$app->get('/tutor/viewProfile/[{tutor_id}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("SELECT first_name, last_name, email, past_high_school, bio FROM `Tutors` WHERE tutor_id=:tutor_id");
+    $sth->bindParam("tutor_id",$args['tutor_id']);
+    $sth->execute();
+    $view = $sth->fetchObject();
+    return $this->response->withJson($view);
+});
+//View Student Profile
+$app->get('/student/viewProfile/[{student_id}]', function ($request, $response, $args) {
+   $sth = $this->db->prepare("SELECT first_name, last_name, email, high_school, graduation_year, bio FROM `Students` WHERE student_id = :student_id");
+   $sth->bindParam("student_id",$args['student_id']);
+   $sth->execute();
+   $view = $sth->fetchObject();
+   return $this->response->withJson($view);
+});
+//View Sessions From Tutor POV
+$app->get('/tutor/sessions/[{tutor_id}]', function ($request, $response, $args) {
+   $sth = $this->db->prepare("SELECT first_name, last_name, bio, isAccepted, time_requested, time_accepted FROM `Sessions` NATURAL JOIN `Students` WHERE tutor_id =  :tutor_id");
+   $sth->bindParam("tutor_id",$args['tutor_id']);
+   $sth->execute();
+   $sessions = $sth->fetchAll();
+   return $this->response->withJson($sessions);
+});
+//View Sessions From Student POV
+$app->get('/student/sessions/[{student_id}]', function ($request, $response, $args) {
+   $sth = $this->db->prepare("SELECT first_name, last_name, bio, isAccepted, time_requested, time_accepted FROM `Sessions` NATURAL JOIN `Tutors` WHERE student_id = :student_id");
+   $sth->bindParam("student_id", $args['student_id']);
+   $sth->execute();
+   $sessions = $sth->fetchAll();
+   return $this->response->withJson($sessions);
 });
