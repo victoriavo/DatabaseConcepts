a46e3c2eea99763dfc55c1876aebe9981d5b0fbf
muralidharm <mmuralidhar@smu.edu>
2017-04-20 23:50:54 +0000

    Edited endpoints for /tutor/viewProfile

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 // Routes
 //View Tutor Profile
 $app->get('/tutor/viewProfile/[{tutor_id}]', function ($request, $response, $args) {
-    $sth = $this->db->prepare("SELECT first_name, last_name, email, past_high_school, bio FROM `Tutors` WHERE tutor_id=:tutor_id");
+    $sth = $this->db->prepare("SELECT photo, first_name, last_name, past_high_school, bio FROM `Tutors`  JOIN `Photos`  WHERE tutor_id=:tutor_id AND tutor_id = id");
     $sth->bindParam("tutor_id",$args['tutor_id']);
     $sth->execute();
     $view = $sth->fetchObject();
@@ -31,4 +31,12 @@ $app->get('/student/sessions/[{student_id}]', function ($request, $response, $ar
    $sth->execute();
    $sessions = $sth->fetchAll();
    return $this->response->withJson($sessions);
+});
+//Find Tutor
+$app->get('/findTutor/[{subject_name}]', function ($request, $response, $args) {
+   $sth = $this->db->prepare("SELECT first_name, last_name, past_high_school FROM `Subjects` NATURAL JOIN `Courses` NATURAL JOIN `Courses Taught` NATURAL JOIN `Tutors`WHERE subject_name = :subject_name");
+   $sth->bindParam("subject_name", $args['subject_name']);
+   $sth->execute();
+   $find = $sth->fetchAll();
+   return $this->response->withJson($find);
 });
