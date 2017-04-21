59c78ff5dd46dbccc4b05186d2b4f3d674e0720e
victoriavo <vickivo11@yahoo.com>
2017-04-20 19:02:24 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,37 +1,5 @@
 <?php
 
-//Get Tutor Profile Info
-    $app->get('/tutor/viewProfile', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM Tutors ORDER BY tutor_id");
-        $sth->execute();
-        $tutorprofiles = $sth->fetchAll();
-        return $this->response->withJson($tutorprofiles);
-    });
-
-//Get Student Profile Info
-$app->get('/student/viewProfile', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM Students ORDER BY student_id");
-        $sth->execute();
-        $studentprofiles = $sth->fetchAll();
-        return $this->response->withJson($studentprofiles);
-    });
-
-//View Sessions from Tutor's pov
-$app->get('/tutor/sessions', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM Sessions NATURAL JOIN Tutors");
-        $sth->execute();
-        $tutorSessions = $sth->fetchAll();
-        return $this->response->withJson($tutorSessions);
-    });
-
-//View Sessions from Student POV
-$app->get('/student/sessions', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM Sessions NATURAL JOIN Students");
-        $sth->execute();
-        $studentSessions = $sth->fetchAll();
-        return $this->response->withJson($studentSessions);
-    });
-
 
 // student sign up NOT TESTED
     $app->post('/student/signup', function ($request, $response) {
