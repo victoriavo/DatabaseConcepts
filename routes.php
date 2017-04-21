f1e299256934cc925f16a93f73b67bb0181d253e
victoriavo <vickivo11@yahoo.com>
2017-04-20 19:59:24 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,34 +1,19 @@
 <?php
-
-
-// student sign up NOT TESTED
-    $app->post('/student/signup', function ($request, $response) {
-        $input = $request->getParsedBody();
-        $sql = "INSERT INTO Students (studentinfo) VALUES (:studentinfo)";
-         $sth = $this->db->prepare($sql);
-        $sth->bindParam("studentinfo", $input['studentinfo']);
+   // tutor sign up 
+    $app->post('/tutor/signup', function ($request, $response) {
+       $input = $request->getParsedBody();
+       $sql = "INSERT INTO `Students`(`first_name`, `last_name`, `email`, `password`) VALUES (:first_name,:last_name,:email,:password)";
+       $sth = $this->db->prepare($sql);
+        $sth->bindParam(":email", $input['email']);
+        $sth->bindParam(":first_name", $input['first_name']);
+         $sth->bindParam(":last_name", $input['last_name']);
+         $sth->bindParam(":password", $input['last_name']);
         $sth->execute();
-        $input['student_id'] = $this->db->lastInsertId();
+        $input['first_name'] = $this->db->lastInsertId();
+        $input['last_name'] = $this->db->lastInsertId();
+        $input['password'] = $this->db->lastInsertId();
+        $input['email'] = $this->db->lastInsertId();
         return $this->response->withJson($input);
     });
-        
 
-// tutor sign up NOT TESTED
-    $app->post('/tutor/signup', function ($request, $response) {
-        $input = $request->getParsedBody();
-        $sql = "INSERT INTO Tutors (tutorinfo) VALUES (:tutorinfo)";
-         $sth = $this->db->prepare($sql);
-        $sth->bindParam("tutorinfo", $input['tutorinfo']);
-        $sth->execute();
-        $input['tutor_id'] = $this->db->lastInsertId();
-        return $this->response->withJson($input);
-    }); 
-    
-//Public Index.phtml page    
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+   
