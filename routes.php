56dec300308836c2205eda76ffdddd3713eb3f8c
Ubuntu <ubuntu@ip-172-31-29-118.us-west-2.compute.internal>
2017-04-26 19:10:07 +0000

    Fixed errors and typos

--- routes.php
+++ routes.php
@@ -24,7 +24,7 @@ $app->post('/tutor/signup', function ($request, $response) {
   $input['password'] = $this->db->lastInsertId();
   $input['email'] = $this->db->lastInsertId();
   return $this->response->withJson($input);
-
+});
 //login
  // Login insert username and password
    $app->post('/login', function ($request, $response) {
@@ -80,23 +80,6 @@ $app->post('/tutor/signup', function ($request, $response) {
          return $this->response->withJson($input);
     });
         
-
-   // tutor sign up 
-    $app->post('/tutor/signup', function ($request, $response) {
-       $input = $request->getParsedBody();
-       $sql = "INSERT INTO `Tutors`(`first_name`, `last_name`, `email`, `password`) VALUES (:first_name,:last_name,:email,:password)";
-       $sth = $this->db->prepare($sql);
-        $sth->bindParam(":email", $input['email']);
-        $sth->bindParam(":first_name", $input['first_name']);
-         $sth->bindParam(":last_name", $input['last_name']);
-         $sth->bindParam(":password", crypt($input['password']));
-        $sth->execute();
-        $input['first_name'] = $this->db->lastInsertId();
-        $input['last_name'] = $this->db->lastInsertId();
-        $input['password'] = $this->db->lastInsertId();
-        $input['email'] = $this->db->lastInsertId();
-        return $this->response->withJson($input);
-    });
  // Logout
     $app->post('/logout', function ($request, $response) {
        $input = $request->getParsedBody();
