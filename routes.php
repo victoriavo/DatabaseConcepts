0421ef34d5955c006bc2131420d2459a41843f54
victoriavo <vickivo11@yahoo.com>
2017-04-25 22:31:38 -0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -32,26 +32,8 @@
         return $this->response->withJson($input);
     });
 
-// tutor sign up 
-$app->post('/tutor/signup', function ($request, $response) {
-  $input = $request->getParsedBody();
-  $sql = "INSERT INTO `Tutors`(`first_name`, `last_name`, `email`, `password`) VALUES (:first_name,:last_name,:email,:password)";
-  $sth = $this->db->prepare($sql);
-  $sth->bindParam(":email", $input['email']);
-  $sth->bindParam(":first_name", $input['first_name']);
-  $sth->bindParam(":last_name", $input['last_name']);
-  $sth->bindParam(":password", $input['last_name']);
-  $sth->execute();
-  $input['first_name'] = $this->db->lastInsertId();
-  $input['last_name'] = $this->db->lastInsertId();
-  $input['password'] = $this->db->lastInsertId();
-  $input['email'] = $this->db->lastInsertId();
-  return $this->response->withJson($input);
-  
 
-});
-
-   // student sign up 
+   // student sign up  
     $app->post('/student/signup', function ($request, $response) {
          $input = $request->getParsedBody();
         $sql = "INSERT INTO `Students`(`first_name`, `last_name`, `email`, `password`) VALUES (:first_name,:last_name,:email,:password)";
@@ -59,15 +41,34 @@ $app->post('/tutor/signup', function ($request, $response) {
          $sth->bindParam(":email", $input['email']);
         $sth->bindParam(":first_name", $input['first_name']);
          $sth->bindParam(":last_name", $input['last_name']);
-         $sth->bindParam(":password", $input['last_name']);
+         $sth->bindParam(":password", crypt($input['password']));
          $sth->execute();
          $input['email'] = $this->db->lastInsertId();
          $input['first_name'] = $this->db->lastInsertId();
          $input['last_name'] = $this->db->lastInsertId();
          $input['password'] = $this->db->lastInsertId();
          return $this->response->withJson($input);
-
     });
+        
+
+   // tutor sign up 
+    $app->post('/tutor/signup', function ($request, $response) {
+       $input = $request->getParsedBody();
+       $sql = "INSERT INTO `Tutors`(`first_name`, `last_name`, `email`, `password`) VALUES (:first_name,:last_name,:email,:password)";
+       $sth = $this->db->prepare($sql);
+        $sth->bindParam(":email", $input['email']);
+        $sth->bindParam(":first_name", $input['first_name']);
+         $sth->bindParam(":last_name", $input['last_name']);
+         $sth->bindParam(":password", crypt($input['password']));
+        $sth->execute();
+        $input['first_name'] = $this->db->lastInsertId();
+        $input['last_name'] = $this->db->lastInsertId();
+        $input['password'] = $this->db->lastInsertId();
+        $input['email'] = $this->db->lastInsertId();
+        return $this->response->withJson($input);
+    });
+
+
 
  // Logout
     $app->post('/logout', function ($request, $response) {
